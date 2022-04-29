6be3ff8614d141635b9364c5d15e745d01703f20
André Junior <andre.souza64@fatec.sp.gov.br>
2022-04-29 12:30:09 -0300

    refactor: adjusting heading with new properties

--- client/src/components/Heading/index.tsx
+++ client/src/components/Heading/index.tsx
@@ -1,10 +1,14 @@
 import { Wrapper } from './styles';
 
+export type LineColors = 'primary' | 'secondary';
+
 export type HeadingProps = {
   children: React.ReactNode;
   color?: 'white' | 'black';
   lineLeft?: boolean;
   lineBottom?: boolean;
+  lineColor?: LineColors;
+  size?: 'small' | 'medium';
 };
 
 export default function Heading({
@@ -12,9 +16,17 @@ export default function Heading({
   color = 'white',
   lineLeft = false,
   lineBottom = false,
+  lineColor = 'primary',
+  size = 'medium',
 }: HeadingProps) {
   return (
-    <Wrapper color={color} lineLeft={lineLeft} lineBottom={lineBottom}>
+    <Wrapper
+      color={color}
+      lineLeft={lineLeft}
+      lineBottom={lineBottom}
+      lineColor={lineColor}
+      size={size}
+    >
       {children}
     </Wrapper>
   );
