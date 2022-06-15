4722082d65478df6232cab6043d8604895f59c6f
André Junior <andre.souza64@fatec.sp.gov.br>
2022-06-15 12:22:08 -0300

    feat: add hook use-wishlist

--- client/src/components/FormProfile/test.tsx
+++ client/src/components/FormProfile/test.tsx
@@ -16,9 +16,6 @@ describe('<FormProfile />', () => {
     expect(
       screen.getByRole('link', { name: /reset password/i })
     ).toBeInTheDocument()
-
-    expect(screen.getByPlaceholderText(/new password/i)).toBeInTheDocument()
-
     expect(screen.getByRole('button', { name: /save/i })).toBeInTheDocument()
   })
 })
