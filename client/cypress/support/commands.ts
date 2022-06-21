e6b7c086b2e88ced903ea5b7caa6496ab8ae3c55
André Junior <andre.souza64@fatec.sp.gov.br>
2022-06-21 13:44:17 -0300

    feat: e2e test finished

--- client/cypress/support/commands.ts
+++ client/cypress/support/commands.ts
@@ -26,6 +26,8 @@
 
 // Add Testing Library Commands
 import '@testing-library/cypress/add-commands';
+import 'cypress-plugin-stripe-elements'
+import { User } from './generate';
 
 Cypress.Commands.add('google', () => cy.visit('http://google.com'))
 
@@ -65,5 +67,55 @@ Cypress.Commands.add('shouldRenderShowcase', ({ name, hightlight = false }) => {
     }
 
     cy.getByDataCy('game-card').should('have.length.gt', 0)
+  })
+})
+
+Cypress.Commands.add('getFields', (fields) => {
+  fields.map(({ label }) => {
+    cy.findByText(label).should('exist')
+  })
+})
+
+Cypress.Commands.add('shouldBeGreaterThan', (value) => {
+  cy
+    .findByText(/^\$\d+(\.\d{1,2})?/)
+    .invoke('text')
+    .then($el => $el.replace('$', ''))
+    .then(parseFloat)
+    .should('be.gt', value)
+})
+
+Cypress.Commands.add('shouldBeLessThan', (value) => {
+  cy
+    .findByText(/^\$\d+(\.\d{1,2})?/)
+    .invoke('text')
+    .then($el => $el.replace('$', ''))
+    .then(parseFloat)
+    .should('be.lt', value)
+})
+
+Cypress.Commands.add('signUp', (user: User) => {
+  cy.findByPlaceholderText(/username/i).type(user.username)
+  cy.findByPlaceholderText(/email/i).type(user.email)
+  cy.findByPlaceholderText(/^password/i).type(user.password)
+  cy.findByPlaceholderText(/confirm password/i).type(user.password)
+  cy.findByRole('button', { name: /sign up now/i }).click()
+})
+
+Cypress.Commands.add('signIn', (email = 'e2e@wongames.com', password = '123456') => {
+  cy.findAllByPlaceholderText(/email/i).type(email)
+  cy.findAllByPlaceholderText(/password/i).type(password)
+  cy.findByRole('button', { name: /sign in now/i }).click()
+})
+
+Cypress.Commands.add('addToCartByIndex', (index) => {
+  cy.getByDataCy('game-card').eq(index).within(() => {
+    cy.findByRole('button', { name: /add to cart/i }).click()
+  })
+})
+
+Cypress.Commands.add('removeFromCartByIndex', (index) => {
+  cy.getByDataCy('game-card').eq(index).within(() => {
+    cy.findByRole('button', { name: /remove from cart/i }).click()
   })
 })
--- client/cypress/support/index.d.ts
+++ client/cypress/support/index.d.ts
@@ -6,6 +6,18 @@ type ShowcaseAttributes = {
   hightlight?: boolean
 }
 
+type FieldsAttributes = {
+  label: string
+  name: string | number
+}
+
+
+type User = {
+  username: string
+  email: string
+  password: string
+}
+
 declare namespace Cypress {
   interface Chainable {
     /**
@@ -31,5 +43,49 @@ declare namespace Cypress {
      * @example cy.shouldRenderShowcase()
      */
     shouldRenderShowcase(atts: ShowcaseAttributes): Chainable<Element>
+
+    /**
+   * Custom command to get fields by label
+   * @example cy.getFields([{ label: 'foo', name: 'foo' }])
+   */
+    getFields(fields: FieldsAttributes[]): Chainable<Element>
+
+    /**
+     * Custom command to sign up
+     * @example cy.signUp(user)
+    */
+    signUp(user: User): Chainable<Element>
+
+    /**
+     * Custom command to check if value is less than
+     * @example cy.shouldBeLessThan(100)
+     */
+    shouldBeLessThan(value: number): Chainable<Element>
+
+    /**
+    * Custom command to check if value is greater than
+    * @example cy.shouldBeGreaterThan(100)
+    */
+    shouldBeGreaterThan(value: number): Chainable<Element>
+
+    /**
+   * Custom command to sign up
+   * @example cy.signIn()
+   */
+    signIn(email?: string, password?: string): Chainable<Element>
+
+
+    /**
+     * Custom command to add game to cart by index
+     * @example cy.addToCartByIndex(1)
+     */
+     addToCartByIndex(value: number): Chainable<Element>
+
+     /**
+      * Custom command to remove game from cart by index
+      * @example cy.removeFromCartByIndex(2)
+      */
+     removeFromCartByIndex(value: number): Chainable<Element>
   }
+
 }
